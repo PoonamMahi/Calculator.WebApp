4f20002b29e6e0ca11f2c3847e14c64bb2764cb1
Kainthsourav <souravkainth@outlook.com>
2022-02-18 21:13:12 +0530

    Add project files.

---
+++ Calculator.Library/CalculatorEvents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculator.Library
+{
+    public class CalculatorEvents
+    {
+        //Addition Task
+        public int Addition(int x,int y)
+        {
+            return x + y; //returns added result
+        }
+        //Subtraction Task
+        public int Subtraction(int x,int y)
+        {
+            return x - y; //returns subtracted result            
+        }
+        //Multiplication Task
+        public int Multiplication(int x,int y)
+        {
+            return x * y; //returns multiplication result 
+        }
+        //Division Task
+        public int Division(int x,int y)
+        {
+            return x / y; //returns division result
+        }
+    }
+}
